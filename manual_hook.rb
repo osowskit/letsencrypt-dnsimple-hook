340da6b1a3792a7ca8b77999683cffd680680f47
Thomas Osowski <osowskit@github.com>
2017-01-29 14:42:32 -0800

    Rewrite to use dnsimple gem

--- manual_hook.rb
+++ manual_hook.rb
@@ -1,62 +1,16 @@
 #!/usr/bin/env ruby
+require 'dnsimple'
 
-require 'resolv'
-require 'uri'
-require 'net/http'
-require 'openssl'
-require 'JSON'
 debug = true
+DNSIMPLE_API_TOKEN = ENV["DNSIMPLE_API_TOKEN"]
 
 if debug
-  DNSIMPLE_TOKEN = # DNS Sandbox Token
+  @client = Dnsimple::Client.new(base_url: "https://api.sandbox.dnsimple.com", access_token: DNSIMPLE_API_TOKEN)
 else
-  DNSIMPLE_TOKEN = # DNS Production Token
+  @client = Dnsimple::Client.new(access_token: DNSIMPLE_API_TOKEN)
 end
 
-if debug
-  @api_host = "https://api.sandbox.dnsimple.com/v2"
-else
-  @api_host = "https://api.dnsimple.com/v2"
-end
-
-def get(url)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Get.new(url)
-  request["authorization"] = "Bearer #{DNSIMPLE_TOKEN}"
-  request["accept"] = 'application/json'
-  request["cache-control"] = 'no-cache'
-
-  response = http.request(request)
-  return JSON.parse(response.body)
-end
-
-def post(url, domain, challenge_txt)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Post.new(url)
-  request["authorization"] = "Bearer #{DNSIMPLE_TOKEN}"
-  request["accept"] = 'application/json'
-  request["cache-control"] = 'no-cache'
-  request["content-type"] = 'application/json'
-
-  json_body = {
-    name: "_acme-challenge.#{domain}",
-    type: "TXT",
-    content: "#{challenge_txt}",
-  }.to_json
-
-  puts json_body
-  request.body = json_body
-
-  response = http.request(request)
-  return JSON.parse(response.body)
-end
-
+# Iterate domain list to return Subdomain and Top-level domains
 def find_domain(account_id, full_domain_name)
 
   return_val = {
@@ -64,13 +18,10 @@ def find_domain(account_id, full_domain_name)
     "subdomain_name" => ""
    }
 
-  # Get top level registered domains
-  url = URI("#{@api_host}/#{account_id}/domains")
-  response = get(url)
+  domains = @client.domains.list_domains(account_id).data
 
-  domains = response['data']
   domains.each do |domain|
-    domain_name = domain["name"]
+    domain_name = domain.name
     if full_domain_name.include? domain_name
       return_val["domain_name"] = domain_name
       return_val["subdomain_name"] = full_domain_name.chomp(".#{domain_name}")
@@ -79,36 +30,14 @@ def find_domain(account_id, full_domain_name)
   return_val
 end
 
-def setup_dns(domain, txt_challenge)
-  resolved = false;
-  singleLoop = false;
-  dns = Resolv::DNS.new;
-  acme_domain = "_acme-challenge."+domain;
-  puts "Checking for pre-existing TXT record for the domain: \"#{acme_domain}\"."
+def setup_dns(account_id, domain, subdomain_name, txt_challenge)
+  acme_domain = "_acme-challenge."+subdomain_name
 
-  until resolved
-    dns.each_resource(acme_domain, Resolv::DNS::Resource::IN::TXT) { |resp|
-     if resp.strings[0] == txt_challenge
-       puts "Found #{resp.strings[0]}. match."
-       resolved = true;
-     else
-       puts "Found #{resp.strings[0]}. no match."
-     end
-    }
-
-    if !resolved
-     if !singleLoop
-       puts "Create TXT record for the domain: \"#{acme_domain}\". TXT record:"
-       puts "\"#{txt_challenge}\""
-       puts "Press enter when DNS has been updated..."
-       $stdin.readline()
-       singleLoop = true
-     end
-
-     puts "Didn't find a match for #{txt_challenge}";
-     puts "Waiting to retry...";
-     sleep 30;
-    end
+  begin
+    @client.zones.create_record(account_id, domain, name: acme_domain, type: "TXT", content: txt_challenge)
+  rescue Dnsimple::RequestError => text
+    # Catch Error 'Zone record already exists'
+    puts text
   end
 end
 
@@ -121,26 +50,19 @@ if __FILE__ == $0
   full_domain_name = ARGV[1]
   txt_challenge = ARGV[3]
 
-  url = URI("#{@api_host}/accounts")
-  response = get(url)
+  account = @client.accounts.list
+  account_id = account.data[0].id
 
-  account_id = response['data'][0]['id']
+  domain_hash = find_domain(account_id, full_domain_name)
+  if domain_hash["domain_name"] != ""
+    response = @client.domains.domain(account_id, domain_hash["domain_name"])
 
-  #verify if account has domain
-  puts domain_hash = find_domain(account_id, full_domain_name)
-
-  #assert value
-
-
-  if hook_stage == "deploy_challenge"
-    domain_name = domain_hash["domain_name"]
-    url = URI("#{@api_host}/#{account_id}/zones/#{domain_name}/records")
-
-    puts response = post(url, domain_hash["subdomain_name"], txt_challenge)
-    #setup_dns(domain, txt_challenge)
-  elsif hook_stage == "clean_challenge"
-    puts "clean"
-    delete_dns(full_domain_name, txt_challenge)
+    if hook_stage == "deploy_challenge"
+      puts "deploy"
+      setup_dns(account_id, domain_hash["domain_name"], domain_hash["subdomain_name"] , txt_challenge)
+    elsif hook_stage == "clean_challenge"
+      puts "clean"
+      delete_dns(full_domain_name, txt_challenge)
+    end
   end
-
 end
