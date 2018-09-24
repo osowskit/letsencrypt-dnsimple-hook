773b3ab5854f721550b495bfbe845c9347a5d6a5
Thomas Osowski <osowskit@github.com>
2018-09-24 10:13:23 -0700

    again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-COMMAND="-c -t dns-01 -d $RENEW_DOMAIN -k ./dnsimple_hook.rb $@"
+COMMAND="-c -t dns-01 -d $RENEW_DOMAIN -k /github/workspace/dnsimple_hook.rb $@"
 
 pwd && ls
 
