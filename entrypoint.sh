f4a42eb1d83700d11c81a90f6e34152cda56f94f
Thomas Osowski <osowskit@github.com>
2018-09-24 10:31:52 -0700

    puts it

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ COMMAND="-c -t dns-01 -d $RENEW_DOMAIN -k /workspace/dnsimple_hook.rb $@"
 
 pwd && ls
 
+ls /
+ls /workspace
+
 /workspace/dehydrated --register --accept-terms
 /workspace/dehydrated "$COMMAND"
