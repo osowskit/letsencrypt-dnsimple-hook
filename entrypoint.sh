ea3dec6e52fb37b270093a08f7f7e6433e4fe944
Thomas Osowski <osowskit@github.com>
2018-09-24 09:28:17 -0700

    debug and correct ruby path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # COMMAND="-X POST -H 'Content-type: application/json' --data '{\"text\":\"Success! $* \"}' $SLACK_WEBHOOK_URL"
 eval ./dehydrated --register --accept-terms
-eval ./dehydrated "-c -t dns-01 -d $RENEW_DOMAIN -k dnsimple_hook.rb"
+eval ./dehydrated "-c -t dns-01 -d $RENEW_DOMAIN -k ./dnsimple_hook.rb"
