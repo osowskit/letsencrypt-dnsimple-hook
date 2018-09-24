3e6328bc9ccc76d572c82f5654b4d08be3c3f050
Thomas Osowski <osowskit@github.com>
2018-09-24 09:49:24 -0700

    re-arrange

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-# COMMAND="-X POST -H 'Content-type: application/json' --data '{\"text\":\"Success! $* \"}' $SLACK_WEBHOOK_URL"
-eval ./dehydrated --register --accept-terms
-eval ./dehydrated "-c -t dns-01 -d $RENEW_DOMAIN -k ./dnsimple_hook.rb"
+COMMAND="-c -t dns-01 -d $RENEW_DOMAIN -k ./dnsimple_hook.rb $@"
+
+./dehydrated --register --accept-terms
+./dehydrated "$COMMAND"
